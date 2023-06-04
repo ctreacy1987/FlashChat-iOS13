53f962b6907dcb5e6587fa8518d74302eea7f112
ctreacy1987 <ctreacy1987@gmail.com>
2023-06-04 20:15:14 +0100

    Added Firebase to project for user auth

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,28 +7,33 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            print("-")
-            print(0.1 * charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            
-            charIndex += 1
-            
-        }
+        titleLabel.text = "⚡️FlashChat"
+        
+//Exmpale of how to implement typing animation using a for loop
+        
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            print("-")
+//            print(0.1 * charIndex)
+//            print(letter)
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//
+//            charIndex += 1
+//
+//        }
        
     }
     
