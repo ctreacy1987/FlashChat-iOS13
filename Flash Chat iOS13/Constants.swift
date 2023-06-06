d293758bc77f296bc70bfbe672cda616178ea913
ctreacy1987 <ctreacy1987@gmail.com>
2023-06-06 18:53:04 +0100

    Added constants file to remove strings from code.

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Ciaran Treacy on 05/06/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    let registerSegue = "RegisterToChat"
+    let loginSegue
+}
